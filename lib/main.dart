7d0c11a55c8a3575104b118ee8ac5d4f2725ee05
nuguideza40 <srisook2000@gmail.com>
2025-07-29 09:50:47 +0700

    Refactor main.dart: streamline imports, update color usage, and improve structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,19 @@
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
-}
+ import 'package:flutter/material.dart' show AppBar, BorderRadius, BoxDecoration, BuildContext, Center, Color, ColorScheme, Colors, Container, EdgeInsets, FloatingActionButton, Icon, Icons, MaterialApp, Scaffold, StatelessWidget, Text, ThemeData, Widget, runApp; // Import ไลบรารี Material ของ Flutter
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     // เมธอด build() ส่งคืนโครงสร้างต้นไม้วิดเจ็ตสำหรับวิดเจ็ตนี้
-    const color = const Color.fromARGB(255, 60, 8, 184);
+    const _ = const Color.fromARGB(255, 60, 8, 184);
     var container = Container(
             padding:const EdgeInsets.all(16.0),
             decoration:BoxDecoration(
-              color:Colors.greenAccent,
+              color:const Color.fromARGB(255, 59, 211, 39),
               borderRadius: BorderRadius.circular(20.0)) ,
-            width: 300.0, 
-            height: 200.0, 
+            width: 300.0,
+            height: 200.0,
             color: Colors.amber);
     var materialApp = MaterialApp(
       theme: ThemeData(
@@ -36,8 +31,6 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: container,
-          
-  
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
@@ -49,5 +42,28 @@ class MyApp extends StatelessWidget {
       ),
     );
     return materialApp;
+  }
+} 
+Future<void> main() async {
+  // จุดเริ่มต้นของแอป Flutter
+  runApp(const MyApp()); // บอก Flutter ว่าวิดเจ็ตใดคือรากฐานของแอป
+}
+
+// MyApp โดยทั่วไปคือวิดเจ็ตรากฐานของแอปพลิเคชันของคุณ
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key}); // Constructor
+
+  @override
+  Widget build(BuildContext context) {
+    // เมธอด build() ส่งคืนโครงสร้างต้นไม้วิดเจ็ตสำหรับวิดเจ็ตนี้
+    return MaterialApp(
+  title: 'My Awesome App',
+  debugShowCheckedModeBanner: false, // ซ่อนแบนเนอร์ debug
+  theme: ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    useMaterial3: true, // ใช้ Material Design เวอร์ชันล่าสุด
+  ),
+  home: const MyHomePage()
+    );
   }
 }
